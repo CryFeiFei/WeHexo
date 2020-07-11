0c082a18f042e98a352ccfa3e3619973579cfcdf
cryfeifei <809151882@qq.com>
2020-07-11 13:00:38 +0800

    添加一个新的页

--- pages/artical/artical.js
+++ pages/artical/artical.js
@@ -1,10 +1,19 @@
+// pages/artical/artical.js
 const app = getApp();
+
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    isLoading: true, // 判断是否尚在加载中
-    article: {} // 内容数据
+
   },
-  onLoad: function() {
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
     var self = this
     wx.request({
       url: 'https://cryfeifei.top/api/articles/Qt释放线程资源的一些工程上的方法.json', //仅为示例，并非真实的接口地址
@@ -35,5 +44,54 @@ Page({
         });
       }
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
